f81efaf25575a7459a3b957fa5803284d6142bd8
Rylan Cole <rylancole@Rylans-Air.hitronhub.home>
2020-06-09 19:24:11 -0700

    updating local option

--- main.py
+++ main.py
@@ -2,27 +2,31 @@
 import praw
 import re
 import os
+import sys
 from datetime import datetime, timedelta
 
-def log(msg):
-    SLACK_URL = os.environ['SLACK_URL']
-    command = os.popen('''curl -X POST -H 'Content-type: application/json' --data '{"text":"'''+msg+'''"}' '''+SLACK_URL)
-    print(command.read())
-    print(command.close())
+def log(msg, local):
+    if(local):
+        print(msg)
+    else: 
+        SLACK_URL = os.environ['SLACK_URL']
+        command = os.popen('''curl -X POST -H 'Content-type: application/json' --data '{"text":"'''+msg+'''"}' '''+SLACK_URL)
+        print(command.read())
+        print(command.close())
 
 
-def main():
+def main(minu, local=False):
     # Create the Reddit instance and login using ./praw.ini data
     reddit = praw.Reddit('wikireplier')
 
     # get current time and correct for how far back we want to look
     _now = datetime.now()
-    _x_time_ago = _now - timedelta(minutes=30)
+    _x_time_ago = _now - timedelta(minutes=minu)
 
     # convert UTC time to a float for comparison
     read_upto_time = _x_time_ago.timestamp()
 
-    # Get the top 10 values from our subreddit
+    # Get the top 20 values from our subreddit
     subreddit = reddit.subreddit('BuyCanadian')
     for submission in subreddit.new(limit=20):
         created_time = submission.created_utc        
@@ -31,10 +35,20 @@ def main():
             # Read link from post and determine if its external
             link = submission.url
             if("reddit" not in link):
-                log("/u/"+str(submission.author)+" posted "+link)
-                log("Check it out here "+submission.permalink)
+                log("/u/"+str(submission.author)+" posted "+link+", check it out <https://www.reddit.com"+submission.permalink+"|here>", local)
         else:
             break
 
 if __name__ == "__main__":
-    main()
+    called = False
+    if(len(sys.argv) > 1):
+        local = False
+        for comm in sys.argv[1:]:
+            if comm in ['-l', '--local']:
+                local = True
+            elif re.match('\d+', comm):
+                main(int(comm), local)
+                called = True
+
+    if not called:
+        main(30, False)
